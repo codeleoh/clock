20c9963438178a3f18e245e07defda1e42f0048d
Leonardo Henrique <code.leoh@gmail.com>
2024-08-20 15:54:46 -0300

    Elementos do relogio digital e os ponteiros do relógio analógico foram selecionados

---
+++ script.js
@@ -0,0 +1,4 @@
+let digitalElement = document.querySelector('.digital');
+let segElement = document.querySelector('.p_s');
+let minElement = document.querySelector('.p_m');
+let horElement = document.querySelector('.p_h');
